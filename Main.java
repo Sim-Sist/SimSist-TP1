8643a7645dfc078b9218d71e4812986138e6bbd2
Agustin <agalarza@itba.edu.ar>
2022-03-14 17:18:03 -0300

    Updated main class.

--- Main.java
+++ Main.java
@@ -7,12 +7,15 @@ import java.util.Random;
 import java.util.TreeMap;
 
 public class Main {
-    private static final double SIZE = 1000;
-    private static final int PARTICLES = 2000000;
-    private static final double RADIUS = 10;
+    private static final double SIZE = 2;
+    private static final int PARTICLES = 8;
+    private static final double RADIUS = .2;
     public static void main(String[] args) {
-        Space s = new Space(100, 50);
-        s.setCriticalRadius(10);
+        Space s = new Space(SIZE);
+        s.setCriticalRadius(RADIUS);
+        s.setRadiusLimits(.1, .2);
+        s.initialize(PARTICLES);
+        System.out.println(s);
         s.calculateCells();
         s.outputInitialState();
         s.outputNeighbours();
@@ -29,8 +32,9 @@ public class Main {
         for (particles = 0; particles < 50000; particles += 1000) {
             List<Double> times = new ArrayList<>();
             for (int i = 0; i < 30; i++) {
-                s = new Space(size, particles);
+                s = new Space(size);
                 s.setCriticalRadius(radius);
+                s.initialize(particles);
                 long start = System.nanoTime();
                 s.calculateCells(gridSize);
                 long end = System.nanoTime();
@@ -43,28 +47,6 @@ public class Main {
         System.out.println("Particles   Elapsed Time");
         elapsedTimes.forEach((k, v) -> System.out.println(String.format("%9d   %12f", k, v)));
 
-        // elapsedTimes = new TreeMap<>();
-        // // Test performance with fixed grid size as 25
-        // gridSize = 25;
-        // for (particles = 1; particles < 100000; particles *= 5) {
-        // List<Double> times = new ArrayList<>();
-        // for (int i = 0; i < 30; i++) {
-        // s = new Space(size, particles);
-        // s.setCriticalRadius(radius);
-        // long start = System.nanoTime();
-        // s.calculateCells(gridSize);
-        // long end = System.nanoTime();
-        // double elapsed = (end - start) / Math.pow(10, 9);
-        // times.add(elapsed);
-        // }
-        // elapsedTimes.put(particles, times.stream().mapToDouble(x ->
-        // x).average().getAsDouble());
-        // }
-        // System.out.println("Performance with increasing particles for grid size of "
-        // + gridSize + ":");
-        // System.out.println("Particles Elapsed Time");
-        // elapsedTimes.forEach((k, v) -> System.out.println(String.format("%9d %12f",
-        // k, v)));
 
     }
 
